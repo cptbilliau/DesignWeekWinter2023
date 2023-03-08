383bcd9c88755d11b9b66863de48b7f723ad55ed
cbilliau <billiau@sheridancollege.ca>
2023-03-08 12:30:38 -0500

    Add varying strike size to hammer and charge timer

--- Hammering/Assets/Scripts/DestroyBlocks.cs
+++ Hammering/Assets/Scripts/DestroyBlocks.cs
@@ -6,6 +6,12 @@ public class DestroyBlocks : MonoBehaviour
 {
     public GameObject DestroyCollider;
     Camera cam;
+    [SerializeField]
+    bool isCharging;
+    [SerializeField]
+    float chargeTimer;
+    [SerializeField]
+    bool goingDown;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,20 +25,64 @@ public class DestroyBlocks : MonoBehaviour
         Mousepos.z = 10f;
         Mousepos = cam.ScreenToWorldPoint(Mousepos);
         Debug.DrawRay(transform.position, Mousepos - transform.position, Color.blue);
+
+
         if (Input.GetMouseButtonDown(0))
         {
+            isCharging = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+
+            
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit, 100))
             {
 
-                Instantiate(DestroyCollider, hit.point, Quaternion.identity);
+                GameObject newObject = Instantiate(DestroyCollider, hit.point, Quaternion.identity);
+                newObject.transform.localScale = new Vector3(chargeTimer, chargeTimer, chargeTimer);
+
+                isCharging = false;
+
                 Debug.Log(hit.transform.position);
 
 
 
             }
+        }
+
+        if (isCharging == true)
+        {
+            
+            if(chargeTimer <= 0)
+            {
+
+                goingDown = false;
+            }
+
+            if (chargeTimer >= 2)
+            {
+                goingDown = true;
+            }
+
+            if (goingDown == true)
+            {
+                chargeTimer -= 2.0f * Time.deltaTime;
+            }
+
+            if (goingDown == false)
+            {
+                chargeTimer += 1.0f * Time.deltaTime;
+            }
+        }
+        if (isCharging == false)
+        {
+
+            chargeTimer = 0;
+            goingDown = false;
         }
     }
 }
--- Hammering/Assets/Scripts/destroySelf.cs
+++ Hammering/Assets/Scripts/destroySelf.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class destroySelf : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +14,7 @@ public class destroySelf : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Destroy(gameObject, 0.02f);
+        Destroy(gameObject, 2f);
         Debug.Log("Destroyed Self");
     }
 }
